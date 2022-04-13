ddec10aa3c7307c109b82574b2c16db7bdfa52fe
Jessica Janine de Oliveira <84152746+JessicaJanine@users.noreply.github.com>
2022-04-13 14:07:03 -0300

    Listando
    Classe fileHelper possui métodos em C# para listar diretórios e para listar arquivos.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using trabalhandoComArquivos.Helper;
+
+namespace trabalhandoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caminho = "C:\\Users\\adrie\\Documents\\Desenvolvimento\\PilaresPOO\\PastaTeste1";
+
+            fileHelper helper = new fileHelper();
+            helper.listarDiretorios(caminho);
+            helper.listarArquivosDiretorios(caminho);
+           
+        }
+    }
+}
+
---
+++ fileHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalhandoComArquivos.Helper
+{
+    public class fileHelper
+    {
+        public void listarDiretorios(string caminho)
+        {
+            var retornoCaminho = Directory.GetDirectories(caminho, "*", SearchOption.AllDirectories);
+
+            foreach (var dir in retornoCaminho)
+            {
+                System.Console.WriteLine(dir);
+            }
+        }
+
+        public void listarArquivosDiretorios(string caminho)
+        {
+            //O * é um filtro para trazer tudo. Mas se quisermos selecionar o tipo de arquivos podemos colocar "*.txt"
+            //Ou seja, "qualquerNome.txt"
+            var retornoArquivos = Directory.GetFiles(caminho, "*", SearchOption.AllDirectories);
+
+            foreach (var dir in retornoArquivos)
+            {
+                System.Console.WriteLine(dir);
+            }
+        }
+    }
+}
